d73522e41413cf98428769a98793345000671d22
Ondrej Certik <ondrej@certik.cz>
2010-11-02 19:26:51 -0700

    Refactor the mesh conversion to a routine

--- femhub/examples/phaml_simple.py
+++ femhub/examples/phaml_simple.py
@@ -8,19 +8,25 @@ from numpy import array
 from phaml import Phaml
 from femhub.plot import plot_mesh_mpl
 
+def convert_mesh(x, y, elems, elems_orders):
+    """
+    Convert the mesh from Phaml representation to femhub representation.
+    """
+    polygons = {}
+    for n, elem in enumerate(elems):
+        polygons[n] = array([ [x[i-1], y[i-1]] for i in elem ])
+    orders = {}
+    for n, order in enumerate(elems_orders):
+        orders[n] = order
+    return polygons, orders
+
 def run():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     domain_file = os.path.join(current_dir, "data", "domain")
     p = Phaml(domain_file)
     p.solve()
-    x, y, _elems, _orders = p.get_mesh()
-    polygons = {}
-    for n, elem in enumerate(_elems):
-        polygons[n] = array([ [x[i-1], y[i-1]] for i in elem ])
-    print polygons
-    orders = {}
-    for n, order in enumerate(_orders):
-        orders[n] = order
+    mesh_data = p.get_mesh()
+    polygons, orders = convert_mesh(*mesh_data)
     import matplotlib
     matplotlib.use("Agg")
     f = plot_mesh_mpl(polygons, orders)
